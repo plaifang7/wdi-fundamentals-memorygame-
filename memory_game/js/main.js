eee6f31e944ef323ccb55a65d781b07de14d67fb
KNIGHT-LYFE <owner1@MacBook-Pro.local>
2018-04-30 17:13:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+// console.log("Up and running!");
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardFour);
+var cards = ["queen", "queen","king","king"];
+cards.length;
+var cardsInPlay = [];
+cardsInPlay.length;
+
+var cardOne = cards[0];
+cardsInPlay.push('queen');
+console.log('User flipped queen');
+var cardTwo = cards[2];
+cardsInPlay.push('king');
+console.log('User flipped king');
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+else{
+	alert("Sorry, try again.");
+}
+}
+
+
