edb813903885bdea9ea8b006cef0eb5c5d57d77c
KNIGHT-LYFE <owner1@MacBook-Pro.local>
2018-04-30 19:14:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,7 +5,28 @@
 // var cardFour = "king";
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardFour);
-var cards = ["queen", "queen","king","king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png',
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png',
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png',
+},
+{
+rank: 'king',
+suit:'diamonds',
+cardImage: 'images/king-of-diamonds.png',
+}
+];
 cards.length;
 var cardsInPlay = [];
 cardsInPlay.length;
@@ -21,8 +42,10 @@ else{
 
 
 var flipcard = function (cardId){
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);	
+console.log("User flipped" + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);	
 if (cardsInPlay.length === 2) {
 	
 checkForMatch();
